861c611a3950a9de26702022548eecee4182cbbb
K-moovie <kyha97@naver.com>
2020-08-11 22:38:26 +0900

    Init Project Settings

---
+++ doruri/src/main/java/kang/kitchen/doruri/DoruriApplication.java
@@ -0,0 +1,13 @@
+package kang.kitchen.doruri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoruriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoruriApplication.class, args);
+    }
+
+}
---
+++ doruri/src/test/java/kang/kitchen/doruri/DoruriApplicationTests.java
@@ -0,0 +1,13 @@
+package kang.kitchen.doruri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoruriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
