85fa46dcc5db2019d8f50dafcaf34c6325405c53
K-moovie <kyha97@naver.com>
2020-08-13 01:41:15 +0900

    Add OAuth2.0 KAKAO API

--- doruri/src/main/java/kang/kitchen/doruri/DoruriApplication.java
+++ doruri/src/main/java/kang/kitchen/doruri/DoruriApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class DoruriApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(DoruriApplication.class, args);
     }
 
---
+++ doruri/src/main/java/kang/kitchen/doruri/OAuth2LoginAuthenticationFilter.java
@@ -0,0 +1,19 @@
+package kang.kitchen.doruri;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class OAuth2LoginAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+    public static final String DEFAULT_FILTER_PROCESSES_URI = "/login/oauth2/code/*";
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+        return null;
+    }
+}
