9b591065139807d763df6d3ce18371a60ab77162
K-moovie <kyha97@naver.com>
2020-08-18 22:04:27 +0900

    Success Login

--- doruri/src/main/java/kang/kitchen/doruri/CustomOAuthProvider.java
+++ doruri/src/main/java/kang/kitchen/doruri/CustomOAuthProvider.java
@@ -13,13 +13,14 @@ public enum CustomOAuthProvider {
                     .authorizationUri("https://kauth.kakao.com/oauth/authorize") // authorization code 얻는 API
                     .tokenUri("https://kauth.kakao.com/oauth/token") // access Token 얻는 API
                     .userInfoUri("https://kapi.kakao.com/v2/user/me") // 유저 정보 조회 API
-                    .clientId("36f934df3a644d54650fb1fc8f9b81ad")
+                    .clientId("2d960674477423e2305b22da427a9338")
                     .clientSecret("YTXwdlZWX144zrdJJvIHrQknJXy6f4dP")
                     .userNameAttributeName("id") // userInfo API Response에서 얻어올 ID 프로퍼티
                     .clientName("Kakao"); // spring 내에서 인식할 OAuth2 Provider Name
         }
     };
 
+    // http://localhost:8080/oauth2/authorization/kakao
     private static final String DEFAULT_LOGIN_REDIRECT_URL = "{baseUrl}/login/oauth2/code/{registrationId}";
 
     protected final ClientRegistration.Builder getBuilder(String registrationId,
