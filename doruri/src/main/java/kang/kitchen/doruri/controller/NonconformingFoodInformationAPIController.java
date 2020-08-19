8b5e02fddf9174209e2a1500c24914db5b581bb1
SWKANG0525 <stardustsw8947@gmail.com>
2020-08-19 14:04:02 +0900

    Fix Bug SERVICE_KEY_NOT_REGISTERED

--- doruri/src/main/java/kang/kitchen/doruri/controller/NonconformingFoodInformationAPIController.java
+++ doruri/src/main/java/kang/kitchen/doruri/controller/NonconformingFoodInformationAPIController.java
@@ -11,19 +11,11 @@ import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLDecoder;
+import java.net.URLEncoder;
 
 @RestController
 public class NonconformingFoodInformationAPIController {
-    String SERVICE_KEY;
-
-    {
-        try {
-            SERVICE_KEY = URLDecoder.decode("zB2dcESUiLERgLWmoOjpdFZj5TCULBlxmRtv7%2B72bA4EUAN1f%2BBF9XA%2BrNClYK6XVtWbytBUiv1AoYF3eizWrw%3D%3D","UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-    }
-
+    String SERVICE_KEY = "zB2dcESUiLERgLWmoOjpdFZj5TCULBlxmRtv7%2B72bA4EUAN1f%2BBF9XA%2BrNClYK6XVtWbytBUiv1AoYF3eizWrw%3D%3D";
     static final String API_END_POINT = "http://apis.data.go.kr/1470000/PrsecImproptFoodInfoService";
     static final String API_FUNC_POINT = "getPrsecImproptFoodList";
     @GetMapping("/api/nonconform")
@@ -31,14 +23,13 @@ public class NonconformingFoodInformationAPIController {
         StringBuffer result = new StringBuffer();
 
         try {
-            String urlstr = API_END_POINT+"/"+API_FUNC_POINT+"?ServiceKey="+SERVICE_KEY+"&Prduct="+prduct;
+            String urlstr = API_END_POINT+"/"+API_FUNC_POINT+"?ServiceKey="+SERVICE_KEY+"&Prduct="+ URLEncoder.encode(prduct,"UTF-8");
             URL url = new URL(urlstr);
-            System.out.println(url.toString());
+
             HttpURLConnection urlconenction = (HttpURLConnection) url.openConnection();
             urlconenction.setRequestMethod("GET");
-
+            System.out.println(urlconenction.getContentEncoding());
             BufferedReader br = new BufferedReader(new InputStreamReader(urlconenction.getInputStream(),"UTF-8"));
-
             String returnLine;
             result.append("<xmp>");
             while((returnLine = br.readLine()) != null) {
